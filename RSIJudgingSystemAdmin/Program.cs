5c65e5dae68c010d841967eb4cdf1af971a2bc1c
Royce Stephan Ignacio <ramil.charles.ignacio@gmail.com>
2017-08-31 17:20:44 +0800

    .

--- RSIJudgingSystemAdmin/Program.cs
+++ RSIJudgingSystemAdmin/Program.cs
@@ -17,6 +17,7 @@ namespace RSIJudgingSystemAdmin
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
+
         }
     }
 }
